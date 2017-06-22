bee45e682268d382861d6a4a529464397e3a7cff
tobi <tolanger94@gmail.com>
2017-06-22 06:41:37 -0700

    day save

--- authentication/serializers.py
+++ authentication/serializers.py
@@ -9,7 +9,7 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'email', 'first_name', 'last_name', 'groups', 'password', 'confirm_password')
+        fields = ('id', 'email', 'first_name', 'last_name', 'password', 'confirm_password')
 
         read_only_fields = ('created_at', 'updated_at')
 
